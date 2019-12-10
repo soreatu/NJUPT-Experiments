267ae566598f02884958ffe4fce4955525318179
soreatu <soreatu@gmail.com>
2019-12-10 23:25:25 +0800

    revise mistakes

--- DiscreatMath/Experiment_2.py
+++ DiscreatMath/Experiment_2.py
@@ -21,7 +21,9 @@ def Transform(n, rlt):
     return mtx
 
 Reflexivity = lambda m: m.diagonal().all()
+Irreflexivity = lambda m: not m.diagonal().any()
 Symmetry = lambda m: (m == m.transpose()).all()
+Irsymmetry = lambda m: not any(x!=y and m[x,y]==True and m[y,x]==True  for x in range(m.shape[0]) for y in range(m.shape[0]))
 Transmission = lambda m: all(m[i, k]==1 for i in range(m.shape[0]) for j in range(m.shape[0]) for k in range(m.shape[0]) if m[i, j]==m[j, k]==1)
 
 def main():
@@ -29,9 +31,9 @@ def main():
         n, rlt = GetInput()
         m = Transform(n, rlt)
         print(f"Reflexivity: {Reflexivity(m)}")
-        print(f"Irreflexivity: {not Reflexivity(m)}")
+        print(f"Irreflexivity: {Irreflexivity(m)}")
         print(f"Symmetry: {Symmetry(m)}")
-        print(f"Irsymmetry: {not Symmetry(m)}")
+        print(f"Irsymmetry: {Irsymmetry(m)}")
         print(f"Transmission: {Transmission(m)}\n")
 
 if __name__ == '__main__':
